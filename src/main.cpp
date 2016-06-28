5e92bd34f00e4e98d359458301b958306dc01061
maxime <maxime@maxime-VirtualBox>
2016-06-29 01:10:59 +0200

    Bug fixe if they are no arguments

--- src/main.cpp
+++ src/main.cpp
@@ -11,13 +11,13 @@
 #include <vector>
 
 #include "Raid.h"
-#include "spdlog/spdlog.h"
-#include "configuration/configuration.cpp"
+//#include "spdlog/spdlog.h"
+#include "configuration/configuration.h"
 
 using namespace std;
 
 int main(int argc, char*argv[]) {
-	namespace spd = spdlog;
+
 	vector<string> 	options;
 	Configuration	config;
 	string 			raidDisk, disk, state, raidName, raidMount, logFile;
@@ -37,60 +37,66 @@ int main(int argc, char*argv[]) {
 
 	for(i=0;i<argc;i++) options.push_back(argv[i]);
 
+	if(argc>1){
+		if(options[1] == "--tagflow"){
+			for(i=1;i<argc;i++){
+				if(options[i] == "--size"){
+					int Aspace, Tspace;
+					md0.statMem(Aspace, Tspace);
+					cout << "Space state :" << endl;
+					cout << "available space : " << Aspace << "GB" << endl;
+					cout << "total space     : " << Tspace << "GB" << endl;
+				}
 
-	if(options[1] == "--tagflow"){
-		for(i=1;i<argc;i++){
-			if(options[i] == "--size"){
-				int Aspace, Tspace;
-				md0.statMem(Aspace, Tspace);
-				cout << "Space state :" << endl;
-				cout << "available space : " << Aspace << "GB" << endl;
-				cout << "total space     : " << Tspace << "GB" << endl;
-			}
-
-			if(options[i] == "--reconstruction-state"){
-				double recovery, finish, speed;
-				md0.rebuildState(recovery, finish, speed);
-				cout << "rebuild state :" << endl;
-				cout << "recovery = " << recovery << endl;
-				cout << "finish   = " << finish << endl;
-				cout << "speed    = " << speed << endl;
+				if(options[i] == "--reconstruction-state"){
+					double recovery, finish, speed;
+					md0.rebuildState(recovery, finish, speed);
+					cout << "rebuild state :" << endl;
+					cout << "recovery = " << recovery << endl;
+					cout << "finish   = " << finish << endl;
+					cout << "speed    = " << speed << endl;
+				}
 			}
 		}
+		else{
+
+			if(options[1] == "Fail"){
+				raidDisk 	= options[2];
+				disk		= options[3];
+
+				md0.diskManipulation(disk, "remove");
+				md0.smartTest(disk, state);
+				if(state == ""){	// not defect disk
+					// md0.diskManipulation(disk, "format");
+					md0.diskManipulation(disk, "add");
+				}
+				else{				// defect disk
+					// !!! NOTIFICATION DANS LE LOG !!!
+					do{				// wait that the disk is change
+						 sleep(30);
+					}while(md0.diskDetection(disk));
+
+					// md0.diskManipulation(disk, "format");
+					md0.diskManipulation(disk, "add");
+				}
+			}
+			/*if(options[1] == "RebuildStarted"){
+				fichier << "rebuild started" << endl;
+				// !!! MOTIFICATION DANS LE LOG !!!
+			}*/
+
+			if(options[1] == "RebuildFinished"){
+				// !!! MOTIFICATION DANS LE LOG !!!
+			}
+
+		}
+		return EXIT_SUCCESS;
 	}
 	else{
-
-		if(options[1] == "Fail"){
-			raidDisk 	= options[2];
-			disk		= options[3];
-
-			md0.diskManipulation(disk, "remove");
-			md0.smartTest(disk, state);
-			if(state == ""){	// not defect disk
-				// md0.diskManipulation(disk, "format");
-				md0.diskManipulation(disk, "add");
-			}
-			else{				// defect disk
-				// !!! NOTIFICATION DANS LE LOG !!!
-				do{				// wait that the disk is change
-					 sleep(30);
-				}while(md0.diskDetection(disk));
-
-				// md0.diskManipulation(disk, "format");
-				md0.diskManipulation(disk, "add");
-			}
-		}
-		/*if(options[1] == "RebuildStarted"){
-			fichier << "rebuild started" << endl;
-			// !!! MOTIFICATION DANS LE LOG !!!
-		}*/
-
-		if(options[1] == "RebuildFinished"){
-			// !!! MOTIFICATION DANS LE LOG !!!
-		}
-
+		cerr << "Error : need argument";
+		return EXIT_FAILURE;
 	}
 
-	return EXIT_SUCCESS;
+
 }
 
