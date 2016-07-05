5aba11d6d93f86417ea11f09135d218381529403
maxime <maxime@maxime-VirtualBox>
2016-07-05 17:26:08 +0200

    Improve management of program options by using boost program_option library and improve management of error

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@
 #include <vector>
 #include <algorithm>
 #include <cctype>
+#include <boost/program_options.hpp>
 
 #include "Raid.h"
 #include "spdlog/spdlog.h"
@@ -20,12 +21,18 @@
 
 int progDetected(std::string program);
 
+namespace po =  boost::program_options;
+
 using namespace std;
+
 
 int main(int argc, char*argv[]) {
 	vector<string> 	options;
 	Configuration	config;
-	int 			i;
+	unsigned int 	i;
+	int				fail = 0;
+	int				rebuildStarted = 0;
+	int 			rebuildFinished = 0;;
 	shared_ptr<spdlog::logger> log;	// var pointer for the log lib
 	string 			raidDisk, disk, state, path;
 	string			raidName, raidMount, logFile, logLevel; 	// for config file
@@ -39,11 +46,56 @@ int main(int argc, char*argv[]) {
 	path = argv[0];
 	path.erase(path.end() - (path.length() - path.find_last_of("/")) + 1, path.end()); // delete path usefull
 
-	// check that the program is launching by root
-	if(getuid() != 0) {
-		cerr << "Error : must be launch by root" << endl;
+	po::options_description opt{"Options"};
+	opt.add_options()
+			("help,h", 					"Help screen")
+			("size,s", 					"Print in the log file the size of the raid")
+			("reconstruction-state,r",	"Print in the log file the state of the reconstruction of disk(s)")
+			("mdadm", po::value<vector<string>>()->
+			 multitoken()->zero_tokens()->composing(), "Mdadm inputs. They are 3 parameters for this mode.\n"
+			 	 	 	 	 	 	 	 	 	 	   "note : the name mdadm is not an argument\n\n"
+					 	 	 	 	 	 	 	 	   "$1 : string with Fail or RebuildsStarted or RebuildFinished \n"
+					 	 	 	 	 	 	 	 	   "$2 : name of the raid disk (system path)\n"
+			 	 	 	 	 	 	 	 	 	 	   "$3 : name of the concerned disk (system path)\n\n"
+			 	 	 	 	 	 	 	 	 	 	   "This mode of the program is calling by mdadm itself.");
+
+	po::positional_options_description popt;
+	popt.add("mdadm", -1);
+
+	po::command_line_parser parser{argc, argv};
+	parser.options(opt).positional(popt).allow_unregistered();
+	po::parsed_options parsed_options = parser.run();
+
+	po::variables_map vm;
+	po::store(parsed_options, vm);
+	po::notify(vm);
+
+	if(vm.count("help")){
+		cout << opt << "\n";
 		return EXIT_FAILURE;
 	}
+	else if(vm.count("size") || vm.count("reconstruction-state")){}
+	else if(vm.count("mdadm")){
+		for(i=0;i<vm["mdadm"].as< vector<string> >().size();i++){	// scan the vector
+			if(vm["mdadm"].as< vector<string> >().at(i).find("Fail") != string::npos) fail = 1;
+			if(vm["mdadm"].as< vector<string> >().at(i).find("RebuildStarted") != string::npos) rebuildStarted = 1;
+			if(vm["mdadm"].as< vector<string> >().at(i).find("RebuildFinished") != string::npos) rebuildFinished = 1;
+			if(vm["mdadm"].as< vector<string> >().at(i).find("/dev") != string::npos){
+				if(i==1) raidDisk = vm["mdadm"].as< vector<string> >().at(i);
+				else if(i==2) disk = vm["mdadm"].as< vector<string> >().at(i);
+			}
+		}
+	}
+	else{	// if no arg
+		cout << opt << "\n";
+		return EXIT_FAILURE;
+	}
+
+	// check that the program is launching by root
+	/*if(getuid() != 0) {
+		cerr << "Error : must be launch by root" << endl;
+		return EXIT_FAILURE;
+	}*/
 
 
 	if(config.Load(path + "raid.conf") == false){				// load the config file
@@ -77,111 +129,98 @@ int main(int argc, char*argv[]) {
 		if(logLevel == spdlog::level::to_str((spdlog::level::level_enum) i)) spdlog::set_level((spdlog::level::level_enum)i);
 	}
 
-	log->alert("-- manage raid has been launch --");
+	log->info("-- manage raid has been launch --");
 
 	// verification that all program use is installed
 	if(progDetected("mdadm")){
-		log->error("Error : smartmontools is not installed");
+		log->error("Mdadm is not installed");
+		cerr << "Mdadm is not installed" << endl;
 		return EXIT_FAILURE;
 	}
 
 	if(progDetected("smartctl")){
-		log->error("Error : smartmontools is not installed");
+		log->error("Smartmontools is not installed");
+		cerr << "Smartmontools is not installed" << endl;
 		return EXIT_FAILURE;
 	}
 
-	for(i=0;i<argc;i++) options.push_back(argv[i]);
 
-	if(argc<=1){
-		log->error("Error launching program : need argument");
-		cerr << "Error : need argument" << endl;;
-		return EXIT_FAILURE;
+	if(vm.count("size")){
+		log->info("ask disk raid space");
+		int Aspace, Tspace;
+		if(md0.statMem(Aspace, Tspace)){
+			log->error("Failed to get information about {}", raidName);
+			cerr << "Failed to get information about " << raidName << endl;
+		}
+		else log->info("disk raid space : available space = {} GB       total space = {} GB", Aspace, Tspace);
+
+		/*cout << "Space state :" << endl;		// uncomment to have information in stdout
+		cout << "available space : " << Aspace << "GB" << endl;
+		cout << "total space     : " << Tspace << "GB" << endl;*/
 	}
 
-	if(options[1] == "--tagflow"){
-		for(i=1;i<argc;i++){
-			if(options[i] == "--size"){
-				log->info("ask disk raid space");
-				int Aspace, Tspace;
-				md0.statMem(Aspace, Tspace);
-				log->info("disk raid space : available space = {} GB       total space = {} GB", Aspace, Tspace);
+	if(vm.count("reconstruction-state")){
+		log->info("ask reconstruction state");
+		double recovery, finish, speed;
+		if(md0.rebuildState(recovery, finish, speed)){
+			log->error("Failed to open /proc/mdstat");
+			cerr << "Failed to open /proc/mdstat" << endl;
+		}
+		else log->info("reconstruction state : rebuild state = {} %       time remaining = {} min       speed = {} Mo/s", recovery, finish, speed);
 
-				/*cout << "Space state :" << endl;		// uncomment to have information in stdout
-				cout << "available space : " << Aspace << "GB" << endl;
-				cout << "total space     : " << Tspace << "GB" << endl;*/
-			}
+		/*cout << "Reconstruction state :" << endl; 	// uncomment to have information in stdout
+		cout << "rebuild state = " << recovery << " %" << endl;
+		cout << "time remaining = " << finish << " min" << endl;
+		cout << "speed = " << speed << " Mo/s" << endl;*/
+	}
 
-			if(options[i] == "--reconstruction-state"){
-				log->info("ask reconstruction state");
-				double recovery, finish, speed;
-				md0.rebuildState(recovery, finish, speed);
-				log->info("reconstruction state : rebuild state = {} %       time remaining = {} min       speed = {} Mo/s", recovery, finish, speed);
 
-				/*cout << "Reconstruction state :" << endl; 	// uncomment to have information in stdout
-				cout << "rebuild state = " << recovery << " %" << endl;
-				cout << "time remaining = " << finish << " min" << endl;
-				cout << "speed = " << speed << " Mo/s" << endl;*/
-			}
+	if(fail){
+		log->alert("disk {} fail", disk);
+
+		log->alert("starting removing of the disk in the raid array");
+		if(md0.diskManipulation(disk, "remove")) log->alert("removing disk fail");
+		else log->alert("removing disk done");
+
+		log->alert("starting the smart test. Please wait 2 min");
+		if(md0.smartTest(disk, state)) log->alert("smart test fail");
+		else log->alert("smart test done");
+
+		if(state == ""){	// not defect disk
+			log->alert("starting formatting disk");
+			if(md0.diskManipulation(disk, "format")) log->alert("format disk fail");
+			else log->alert("format disk done");
+
+			log->alert("starting adding disk in the raid array");
+			if(md0.diskManipulation(disk, "add")) log->alert("adding disk fail");
+			else log->alert("adding disk done");
+		}
+		else{				// defect disk
+			log->emerg("Disk {} is defect. Please change the disk", disk);
+
+			do{				// wait that the disk is change
+				 sleep(30);
+			}while(md0.diskDetection(disk));
+			log->emerg("New disk detected");
+
+			log->emerg("starting formatting disk");
+			if(md0.diskManipulation(disk, "format")) log->emerg("format disk fail");
+			else log->emerg("format disk done");
+
+			log->emerg("starting adding disk in the raid array");
+			if(md0.diskManipulation(disk, "add")) log->emerg("adding disk fail");
+			else log->emerg("adding disk done");
 		}
 	}
-	else{
-		transform(options[1].begin(), options[1].end(), options[1].begin(), ::tolower);
-
-		if(options[1].find("fail") != string::npos){
-			raidDisk 	= options[2];
-			disk		= options[3];
-
-			log->alert("disk {} fail", disk);
-
-			log->alert("starting removing of the disk in the raid array");
-			if(md0.diskManipulation(disk, "remove")) log->alert("removing disk fail");
-			else log->alert("removing disk done");
-
-			log->alert("starting the smart test. Please wait 2 min");
-			if(md0.smartTest(disk, state)) log->alert("smart test fail");
-			else log->alert("smart test done");
-
-			if(state == ""){	// not defect disk
-				log->alert("starting formatting disk");
-				if(md0.diskManipulation(disk, "format")) log->alert("format disk fail");
-				else log->alert("format disk done");
-
-				log->alert("starting adding disk in the raid array");
-				if(md0.diskManipulation(disk, "add")) log->alert("adding disk fail");
-				else log->alert("adding disk done");
-			}
-			else{				// defect disk
-				log->emerg("Disk {} is defect. Please change the disk", disk);
-
-				do{				// wait that the disk is change
-					 sleep(30);
-				}while(md0.diskDetection(disk));
-				log->emerg("New disk detected");
-
-				log->emerg("starting formatting disk");
-				if(md0.diskManipulation(disk, "format")) log->emerg("format disk fail");
-				else log->emerg("format disk done");
-
-				log->emerg("starting adding disk in the raid array");
-				if(md0.diskManipulation(disk, "add")) log->emerg("adding disk fail");
-				else log->emerg("adding disk done");
-			}
-		}
-		else if(options[1] == "rebuildstarted"){
-			log->info("Rebuild started");
-		}
-
-		else if(options[1] == "rebuildfinished"){
-			log->info("Rebuild finished");
-		}
-		else{
-			log->error("Error launching program : need valid argument");
-			cerr << "Error : need valid argument" << endl;;
-			return EXIT_FAILURE;
-		}
+	else if(rebuildStarted){
+		log->info("Rebuild started");
 	}
+
+	else if(rebuildFinished){
+		log->info("Rebuild finished");
+	}
+
 	return EXIT_SUCCESS;
-
 }
 
 int progDetected(std::string program){
@@ -195,4 +234,5 @@ int progDetected(std::string program){
 	if(error.find("execvp") != string::npos) return EXIT_FAILURE;
 	else return EXIT_SUCCESS;
 }
+
 
